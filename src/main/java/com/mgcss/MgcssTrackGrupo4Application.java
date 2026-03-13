879a99067532d9826d389518b2cc5e02fb95535c
Tino <tino.garciapedrero@gmail.com>
2026-03-13 13:19:51 +0100

    create project

---
+++ src/main/java/com/mgcss/MgcssTrackGrupo4Application.java
@@ -0,0 +1,13 @@
+package com.mgcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgcssTrackGrupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgcssTrackGrupo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgcss/MgcssTrackGrupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgcssTrackGrupo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
