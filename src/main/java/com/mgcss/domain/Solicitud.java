1593eaecfe60df6c37f54f73fc5e7d4e7edae3a7
Tino <tino.garciapedrero@gmail.com>
2026-03-27 12:54:21 +0100

    Test: Intentamos 80% coverage

--- src/main/java/com/mgcss/domain/Solicitud.java
+++ src/main/java/com/mgcss/domain/Solicitud.java
@@ -4,36 +4,83 @@ import java.time.LocalDateTime;
 
 public class Solicitud {
 
+    public enum Estado {
+        ABIERTA, CERRADA, EN_PROCESO
+    };
+
     private Long id;
-    private String estado;
-    private LocalDateTime fechaCreacion;
+    private String descripcion;
+    private final LocalDateTime fechaCreacion;
+    private Estado estado_actual;
+    private Tecnico tecnicoAsignado;
+    private LocalDateTime fechaCierre;
 
     public Solicitud() {
         this.fechaCreacion = LocalDateTime.now();
-        this.estado = "ABIERTA";
+        this.fechaCierre = null;
+        this.estado_actual = Estado.ABIERTA;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
+    public Estado getEstado() {
+        return estado_actual;
     }
 
     public LocalDateTime getFechaCreacion() {
         return fechaCreacion;
     }
 
-    public void setFechaCreacion(LocalDateTime fechaCreacion) {
-        this.fechaCreacion = fechaCreacion;
+    public String getDescripcion() {
+        return descripcion;
     }
+
+    public boolean setDescripcion(String descripcion) {
+        if (estado_actual != Estado.CERRADA) {
+            this.descripcion = descripcion;
+            return true;
+        }
+        return false;
+    }
+
+    public Estado getEstado_actual() {
+        return estado_actual;
+    }
+
+    public LocalDateTime getFechaCierre() {
+        return fechaCierre;
+    }
+
+    public Tecnico getTecnicoAsignado() {
+        return tecnicoAsignado;
+    }
+
+    public boolean asignarTecnico(Tecnico tecnicoAsignado) {
+        if ((Estado.ABIERTA.equals(this.estado_actual) || Estado.EN_PROCESO.equals(this.estado_actual)) && tecnicoAsignado.isActivo()) {
+            this.tecnicoAsignado = tecnicoAsignado;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean iniciarProceso() {
+        if (Estado.ABIERTA.equals(this.estado_actual) && this.tecnicoAsignado != null) {
+            this.estado_actual = Estado.EN_PROCESO;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean cerrar() {
+        if (Estado.EN_PROCESO.equals(this.estado_actual)) {
+            this.estado_actual = Estado.CERRADA;
+            this.fechaCierre = LocalDateTime.now();
+            return true;
+        }
+        return false;
+    }
+
 }
---
+++ src/main/java/com/mgcss/domain/Tecnico.java
@@ -0,0 +1,63 @@
+package com.mgcss.domain;
+
+public class Tecnico {
+
+    public enum Especialidad {HARDWARE, SOFTWARE, REDES};
+
+    private Long id;
+    private String nombre;
+    private Especialidad especialidad;
+    private boolean activo;
+
+    public Tecnico(String nombre, Especialidad especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.activo = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public boolean activar(){
+        if (!activo) {
+            this.activo = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean desactivar(){
+        if (activo) {
+            this.activo = false;
+            return true;
+        }
+        return false;
+    }
+    
+
+}
